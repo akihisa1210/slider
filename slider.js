26b8538eb8fec8fb866b173c5004b6a4229c4f13
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2018-11-04 17:07:01 +0900

    Enable to resize textarea automatically

--- slider.js
+++ slider.js
@@ -1,3 +1,11 @@
+/**
+ * 以下の処理を実行する。
+ * 1. テキストエリアの keyup イベントを監視する
+ * 2. テキストエリアの内容を取得する
+ * 3. marked にパースさせる
+ * 4. パース結果を HTML として出力する
+ * 5. highlight.js でコードブロックをハイライトする
+ */
 $(function() {
     marked.setOptions({
         langPrefix: ''
@@ -15,3 +23,15 @@ $(function() {
         });
     });
 });
+
+/**
+ * テキストエリアのサイズを、入力に応じて拡張する。
+ */
+$(function() {
+    var $editor = $('#editor');
+    var lineHeight = parseInt($editor.css('lineHeight'));
+    $editor.on('input', function(e) {
+      var lines = ($(this).val() + '\n').match(/\n/g).length;
+      $(this).height(lineHeight * lines);
+    });
+  });
